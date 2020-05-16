ee8ab51f700f312fc4f70462fd683475a055eccc
emptypockets <andrey@dsrp.tv>
2020-05-16 09:56:31 -0400

    Updates

---
+++ precache-manifest.2a1649bad610faa63fb7b2dab2f0bbef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "622cb60ba4d61108c0cdbc56f276de55",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c9fb25fba592a56a4cf1",
+    "url": "/monsters-rolodex/static/css/main.2edbfb0d.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9fb25fba592a56a4cf1",
+    "url": "/monsters-rolodex/static/js/main.fae95826.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
